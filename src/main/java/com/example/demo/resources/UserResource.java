171a276b6027c5e5c58c911b39a20d18cd90db2f
Rafael Brito <rafabrito.office@gmail.com>
2020-07-09 16:58:04 -0300

    Update de um Usuário

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,17 +42,23 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@PostMapping // Mapeamento para o Postman
+	@PostMapping // Inserindo dados com o Rest vindo do Postman
 	public ResponseEntity<User> insert(@RequestBody User obj){ // RequestBody o JSON que chegou no webservice vai ser convertido para um objeto java, ou seja, ele deixa de ser uma string (json) para ser um objeto java (classe)
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // Necessário para o http uri pro código 201
 		return ResponseEntity.created(uri).body(obj); // Trazendo o código 201 para inserção de um user 
 	}
 	
-	@DeleteMapping (value = "/{id}")
+	@DeleteMapping (value = "/{id}") // Deletando dados com o Rest vindo do Postman
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}") // Atualizando dados com o Rest vindo do Postman
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj ){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -23,7 +23,7 @@ public class UserService {
 	
 	//Recuperando Usuário por ID
 	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id); // o findById retorna um Optional
+		Optional<User> obj = repository.findById(id); // o findById retorna um Optional, indo até o banco de dados
 		return obj.get();
 	}
 	
@@ -33,5 +33,17 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id); // retorna um obj sem necessitar ir no banco de dados
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
