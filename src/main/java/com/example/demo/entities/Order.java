f126223414b34ff5b076174d2403df812626577e
Rafael Brito <rafabrito.office@gmail.com>
2020-07-09 15:58:20 -0300

    Métodos SubTotal e Total

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable{
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem orderItem : items) {
+			sum = sum + orderItem.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
--- src/main/java/com/example/demo/entities/OrderItem.java
+++ src/main/java/com/example/demo/entities/OrderItem.java
@@ -62,7 +62,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
+	
+	public Double getSubTotal(){
+		return price * quantity;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
